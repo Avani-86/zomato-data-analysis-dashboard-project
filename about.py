ccd9e0ae37be6f379a6a0d8abf07716ab2f5a068
Awantika Shivhare <125693319+Avani-86@users.noreply.github.com>
2026-06-06 20:12:45 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,46 @@
+
+import streamlit as st
+
+st.title("Know More About Us")
+
+import streamlit as st
+
+st.markdown(" About This Project ")
+st.markdown("""
+            ## Project Overview
+This project analyzes *Zomato restaurant data* to uncover insights about customer preferences, cost trends, and city-wise restaurant distribution.
+            
+### Objectives
+
+- Identify top cuisines and cities for dining.
+
+- Understand factors influencing restaurant ratings.
+
+- Visualize cost and popularity trends across India.
+
+### Tools Used
+
+- *Python* (Pandas, NumPy)
+- *Matplotlib* *Seaborn, **Plotly*
+- *Streamlit* for interactive visualization
+
+### Dataset
+
+Data Source: [Zomato Dataset
+
+(Kaggle)](https://www.kaggle.com/datasets)
+
+- Columns: Restaurant Name, City, Rating, Cost for Two, Cuisines, Votes, etc.
+
+
+
+            
+
+### Author
+
+*Awantika Shivhare*
+
+📧avanishivhare001@gmail.com
+
+💻[GitHub]    
+            """)
